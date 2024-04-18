9eae6115c6524779185f9a7bb9071f653ae693f7
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-18 17:56:47 +0000

    update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
-"""
-Duck typing - Annotating the first element of a sequence
-"""
+"""Augmenting the code with the correct duck-typed annotations:"""
+from typing import Sequence, Any, Union, Optional
 
-from typing import Sequence, Any, Union
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-    Returns the first element of the input sequence, or None if the sequence is empty.
-
-    Args:
-        lst (Sequence[Any]): A sequence of elements of any type.
-
-    Returns:
-        Union[Any, None]: The first element of the sequence, or None if the sequence is empty.
-    """
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """Returns the first element of the sequence or None if
+    the sequence is empty."""
     if lst:
         return lst[0]
     else:
         return None
-
