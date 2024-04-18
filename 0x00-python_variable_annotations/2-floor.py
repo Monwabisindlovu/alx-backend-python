ba7e82e419b06b69e1cc34e3f3277c41852a48e2
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-18 17:12:29 +0000

    updste

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import math
+from 2-floor import floor
+
+ans = floor(3.14)
+
+print(ans == math.floor(3.14))
+print(floor.__annotations__)
+print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
