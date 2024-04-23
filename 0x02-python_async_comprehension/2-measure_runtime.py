f5410865eae9dac7150f03b2e814c0b7b3e7d9ef
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-04-23 10:07:49 +0000

    2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 """
-Defines a concurrency of 4 Asynchronous Comprehension operations
+From the previous file, import wait_n into 2-measure_runtime.py.
+
+Create a measure_time function
+with integers n and max_delay as arguments
+that measures the total execution time for wait_n(n, max_delay),
+and
+returns total_time / n.
+Your function should return a float.
+
+Use the time module to measure an approximate elapsed time.
 """
+
 import time
 import asyncio
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_runtime() -> float:
-    """
-    Collect 10 random numbers using an async comprehensing,
-    then return the 10 random numbers.
-    """
-    start: float = time.perf_counter()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end: float = time.perf_counter()
-    return (end - start)
+def measure_time(n: int, max_delay: int) -> float:
+    """ This coroutine measures the total runtime of executing async_comprehension four times in parallel. """
+
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / ni
